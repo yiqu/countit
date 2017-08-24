92d531d7b5f6d5e29ba7dfcea109910567240393
Kevin Qu <kevinqu88@gmail.com>
2017-08-24 07:57:14 -0400

    refactoring

--- src/app/body-area/body.component.ts
+++ src/app/body-area/body.component.ts
@@ -2,17 +2,26 @@ import { Component, OnInit } from '@angular/core';
 // App version
 const { version: appVersion } = require('../../../package.json')
 
+
+/**
+ * Component class for the app. The body component consists of 
+ * 2 other components, input-area and result-area.
+ */
 @Component({
   moduleId: module.id,
   selector: 'body-component',
   templateUrl: 'body.component.html',
   styleUrls: ['./body.component.css']
 })
-
 export class BodyComponent {
   pageTitle: string = "Text Analyzer"
   appVersion: string;
 
+  /**
+   * Constructor
+   * 
+   * Set up version on start.
+   */
   constructor() {
     this.appVersion = appVersion
   }
