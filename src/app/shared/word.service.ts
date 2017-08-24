7da4bd46ad86b0c519c33f4b9a9537c1933d8801
Kevin Q <kevinqu88@gmail.com>
2017-08-23 21:45:07 -0400

    refactor

--- src/app/shared/word.service.ts
+++ src/app/shared/word.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { WordDetail } from './word.model';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { WordDetail } from './word.model';
 
 /**
  * Injectable Word Service. Calculations are done here.
